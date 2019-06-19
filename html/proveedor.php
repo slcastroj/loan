518656e58ddaa1160231292113f0c187744f547c
Israel Osorio <karouse123@hotmail.com>
2019-06-18 22:01:31 -0400

    aa

--- html/proveedor.php
+++ html/proveedor.php
@@ -15,7 +15,7 @@ use models\models\ProveedorQuery;
 </head>
 <body>
     <main class="container-fluid">
-        <div class="row mt-5 m-lg-5">
+        <div class="row mt-5 m-lg-5"> 
             <form action="" class="col-12 col-lg-6">
             <div class="row">
                 <div class="form-group col-12 col-lg-6">
