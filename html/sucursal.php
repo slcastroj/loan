3676705e0c2154bf503f7b935dd348b8e54abf1c
Israel Osorio <karouse123@hotmail.com>
2019-06-18 22:12:41 -0400

    sucursal

---
+++ html/sucursal.php
@@ -0,0 +1,61 @@
+<?php
+include_once("../vendor/autoload.php");
+include_once("../generated-conf/config.php");
+
+
+use models\models\SucursalQuery;
+
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once("../templates/imports.php"); ?>
+    <title>Índice</title>
+</head>
+<body>
+    <main class="container-fluid">
+        <div class="row mt-5 m-lg-5"> 
+            <form action="" class="col-12 col-lg-6">
+            <div class="row">
+                <div class="form-group col-12 col-lg-6">
+                    <label for="txtNombre">Nombre:</label>
+                    <input type="text" name="nombre" id="txtNombre" class="form-control">
+                </div>
+                <div class="form-group col-12 col-lg-6">
+                    <label for="pckActivo">Activo:</label>
+                    <select name="activo" id="pckActivo" class="form-control">
+                        <option value="">Si</option>
+                        <option value="">No</option>
+                    </select>
+                </div>
+                <div class="text-right col-12">
+                    <button class="btn btn-outline-danger col-12 col-lg-4 btn-sm mb-3 mb-lg-0">Limpiar campos</button>
+                    <input type="submit" class="btn btn-primary col-12 col-lg-4" value="Agregar">
+                </div>
+            </div>
+            </form>
+            <div class="col-12 col-lg-6 p-5" style="height:500px; overflow-y:scroll">
+                <?php
+                $Sucursales = SucursalQuery::create()->find();
+
+                foreach ($Sucursales as $sucursal) {
+                    $nombre = $sucursal->getNombre();
+                    $activo = $sucursal->getActivo() != 0 ? 'Si' : 'No';                    
+
+                    echo "<div class=\"row border rounded p-2 mb-3\">
+                    <span class=\"text-primary col-12\">$nombre</span>
+                    <div class=\"col-12\">
+                        <span class=\"text-secondary\">Activo: $activo</span>
+                        </div>
+                    <div class=\"col-12 text-right\">
+                        <button class=\"btn btn-danger col-12 col-lg-4 btn-sm mb-3 mb-lg-0\">Eliminar</button>
+                    </div>
+                </div>";
+                }
+                ?>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
