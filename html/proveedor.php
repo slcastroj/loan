50a988e015a158364e0436dfe5c56188fb63687b
slcastroj <sl.castroj@gmail.com>
2019-06-18 22:01:56 -0400

    Fusion

--- html/proveedor.php
+++ html/proveedor.php
@@ -6,7 +6,7 @@ include_once("../generated-conf/config.php");
 use models\models\ProveedorQuery;
 
 ?>
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
